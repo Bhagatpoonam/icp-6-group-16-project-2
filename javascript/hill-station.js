05eef2f7ae7a4fd106ffe12fb84cb1065ade11dd
Bhagatpoonam <bhagatpoonam61@gmail.com>
2023-12-22 23:13:43 -0800

    Hill-station page complete

--- javascript/hill-station.js
+++ javascript/hill-station.js
@@ -7,22 +7,22 @@
     
             const hillElment= document.getElementById('hills')
             if(num === 1){
-                hillElment.src='../images/Hill station/leh ladak.jpg'
+                hillElment.src='../images/hill-station/leh ladak.jpg'
             }
             else if(num ===2){
-                hillElment.src ='../images/Hill station/leh ladak2.jpg';
+                hillElment.src ='../images/hill-station/leh ladak2.jpg';
             }
             else if(num ===3){
-                hillElment.src ='../images/Hill station/leh ladak3.jpg';
+                hillElment.src ='../images/hill-station/leh ladak3.jpg';
             }
             else if(num === 4){
-                hillElment.src ='../images/Hill station/leh ladak1.webp';
+                hillElment.src ='../images/hill-station/leh ladak1.webp';
             }
             else if(num === 5){
-                hillElment.src ='../images/Hill station/leh ladak6.jpg';
+                hillElment.src ='../images/hill-station/leh ladak6.jpg';
             }
             else if(num === 6){
-                hillElment.src ='../images/Hill station/leh ladak7.webp';
+                hillElment.src ='../images/hill-station/leh ladak7.webp';
             }
         }
           function next2(){
@@ -32,22 +32,22 @@
             const num = Math.floor(Math.random () * (max-min)) + min;
             const hill1Elment= document.getElementById('hills1')
             if(num === 1){
-                hill1Elment.src='../images/Hill station/munnar1.jpg'
+                hill1Elment.src='../images/hill-station/munnar1.jpg'
             }
             else if(num ===2){
-                hill1Elment.src ='../images/Hill station/munnar2.jpg';
+                hill1Elment.src ='../images/hill-station/munnar2.jpg';
             }
             else if(num ===3){
-                hill1Elment.src ='../images/Hill station/munnar3.jpg';
+                hill1Elment.src ='../images/hill-station/munnar3.jpg';
             }
             else if(num === 4){
-                hill1Elment.src ='../images/Hill station/munnar4.jpg';
+                hill1Elment.src ='../images/hill-station/munnar4.jpg';
             }
             else if(num === 5){
-                hill1Elment.src ='../images/Hill station/kerala.jpeg';
+                hill1Elment.src ='../images/hill-station/kerala.jpeg';
             }
             else if(num === 6){
-                hill1Elment.src ='../images/Hill station/munnar6.jpg';
+                hill1Elment.src ='../images/hill-station/munnar6.jpg';
             }
         }
         
@@ -59,22 +59,22 @@
             const num = Math.floor(Math.random () * (max-min)) + min;
             const hill2Elment= document.getElementById('hills2')
             if(num === 1){
-                hill2Elment.src='../images/Hill station/ooty1.jpg'
+                hill2Elment.src='../images/hill-station/ooty1.jpg'
             }
             else if(num ===2){
-                hill2Elment.src ='../images/Hill station/ooty2.avif';
+                hill2Elment.src ='../images/hill-station/ooty2.avif';
             }
             else if(num ===3){
-                hill2Elment.src ='../images/Hill station/ooty3.webp';
+                hill2Elment.src ='../images/hill-station/ooty3.webp';
             }
             else if(num === 4){
-                hill2Elment.src ='../images/Hill station/ooty4.jpg';
+                hill2Elment.src ='../images/hill-station/ooty4.jpg';
             }
             else if(num === 5){
-                hill2Elment.src ='../images/Hill station/ooty5.jpg';
+                hill2Elment.src ='../images/hill-station/ooty5.jpg';
             }
             else if(num === 6){
-                hill2Elment.src ='../images/Hill station/ooty6.jpeg';
+                hill2Elment.src ='../images/hill-station/ooty6.jpeg';
             }
         }
     
@@ -85,22 +85,22 @@
             const num = Math.floor(Math.random () * (max-min)) + min;
             const hill3Elment= document.getElementById('hills3')
             if(num === 1){
-                hill3Elment.src='../images/Hill station/manali1.jpg'
+                hill3Elment.src='../images/hill-station/manali1.jpg'
             }
             else if(num ===2){
-                hill3Elment.src ='../images/Hill station/manali3.cms';
+                hill3Elment.src ='../images/hill-station/manali3.cms';
             }
             else if(num ===3){
-                hill3Elment.src ='../images/Hill station/manali2.jpg';
+                hill3Elment.src ='../images/hill-station/manali2.jpg';
             }
             else if(num === 4){
-                hill3Elment.src ='../images/Hill station/manali4.jpg';
+                hill3Elment.src ='../images/hill-station/manali4.jpg';
             }
             else if(num === 5){
-                hill3Elment.src ='../images/Hill station/manali5.jpeg';
+                hill3Elment.src ='../images/hill-station/manali5.jpeg';
             }
             else if(num === 6){
-                hill3Elment.src ='../images/Hill station/manali6.jpg';
+                hill3Elment.src ='../images/hill-station/manali6.jpg';
             }
         }
         function next5(){
@@ -110,22 +110,22 @@
             const num = Math.floor(Math.random () * (max-min)) + min;
             const hill4Elment= document.getElementById('hills4')
             if(num === 1){
-                hill4Elment.src='../images/Hill station/naintal1.png'
+                hill4Elment.src='../images/hill-station/naintal1.png'
             }
             else if(num ===2){
-                hill4Elment.src ='../images/Hill station/naintal2.jpg';
+                hill4Elment.src ='../images/hill-station/naintal2.jpg';
             }
             else if(num ===3){
-                hill4Elment.src ='../images/Hill station/naintal3.cms';
+                hill4Elment.src ='../images/hill-station/naintal3.cms';
             }
             else if(num === 4){
-                hill4Elment.src ='../images/Hill station/naintal4.webp';
+                hill4Elment.src ='../images/hill-station/naintal4.webp';
             }
             else if(num === 5){
-                hill4Elment.src ='../images/Hill station/naintal5.jpg';
+                hill4Elment.src ='../images/hill-station/naintal5.jpg';
             }
             else if(num === 6){
-                hill4Elment.src ='../images/Hill station/nainital.jpg';
+                hill4Elment.src ='../images/hill-station/nainital.jpg';
             }
         }
         function next6(){
@@ -135,22 +135,22 @@
             const num = Math.floor(Math.random () * (max-min)) + min;
             const hill5Elment= document.getElementById('hills5')
             if(num === 1){
-                hill5Elment.src='../images/Hill station/maha1.jpg'
+                hill5Elment.src='../images/hill-station/maha1.jpg'
             }
             else if(num ===2){
-                hill5Elment.src ='../images/Hill station/maha2.webp';
+                hill5Elment.src ='../images/hill-station/maha2.webp';
             }
             else if(num ===3){
-                hill5Elment.src ='../images/Hill station/maha3.webp';
+                hill5Elment.src ='../images/hill-station/maha3.webp';
             }
             else if(num === 4){
-                hill5Elment.src ='../images/Hill station/maha4.jpg';
+                hill5Elment.src ='../images/hill-station/maha4.jpg';
             }
             else if(num === 5){
-                hill5Elment.src ='../images/Hill station/maha5.jpg';
+                hill5Elment.src ='../images/hill-station/maha5.jpg';
             }
             else if(num === 6){
-                hill5Elment.src ='../images/Hill station/maha1.jpg';
+                hill5Elment.src ='../images/hill-station/maha1.jpg';
             }
         }
         function next1(){
@@ -160,22 +160,22 @@
             const num = Math.floor(Math.random () * (max-min)) + min;
             const hill11Elment= document.getElementById('hills11')
             if(num === 1){
-                hill11Elment.src='../images/Hill station/gangtok1.jpg'
+                hill11Elment.src='../images/hill-station/gangtok1.jpg'
             }
             else if(num ===2){
-                hill11Elment.src ='../images/Hill station/gangtok.webp';
+                hill11Elment.src ='../images/hill-station/gangtok.webp';
             }
             else if(num ===3){
-                hill11Elment.src ='../images/Hill station/gangtok3.cms';
+                hill11Elment.src ='../images/hill-station/gangtok3.cms';
             }
             else if(num === 4){
-                hill11Elment.src ='../images/Hill station/gangtok4.jpg';
+                hill11Elment.src ='../images/hill-station/gangtok4.jpg';
             }
             else if(num === 5){
-                hill11Elment.src ='../images/Hill station/gangtok5.jpg';
+                hill11Elment.src ='../images/hill-station/gangtok5.jpg';
             }
             else if(num === 6){
-                hill11Elment.src ='../images/Hill station/gangtok.webp';
+                hill11Elment.src ='../images/hill-station/gangtok.webp';
             }
         }
         function next12(){
@@ -185,22 +185,22 @@
             const num = Math.floor(Math.random () * (max-min)) + min;
             const hill12Elment= document.getElementById('hills12')
             if(num === 1){
-                hill12Elment.src='../images/Hill station/matheran1.jpg'
+                hill12Elment.src='../images/hill-station/matheran1.jpg'
             }
             else if(num ===2){
-                hill12Elment.src ='../images/Hill station/matheran3.webp';
+                hill12Elment.src ='../images/hill-station/matheran3.webp';
             }
             else if(num ===3){
-                hill12Elment.src ='../images/Hill station/matheran2.png';
+                hill12Elment.src ='../images/hill-station/matheran2.png';
             }
             else if(num === 4){
-                hill12Elment.src ='../images/Hill station/matheran5.webp';
+                hill12Elment.src ='../images/hill-station/matheran5.webp';
             }
             else if(num === 5){
-                hill12Elment.src ='../images/Hill station/matheran7.jpg';
+                hill12Elment.src ='../images/hill-station/matheran7.jpg';
             }
             else if(num === 6){
-                hill12Elment.src ='../images/Hill station/matheran6.webp';
+                hill12Elment.src ='../images/hill-station/matheran6.webp';
             }
         }
         function next10(){
@@ -210,22 +210,22 @@
             const num = Math.floor(Math.random () * (max-min)) + min;
             const hill10Elment= document.getElementById('hills10')
             if(num === 1){
-                hill10Elment.src='../images/Hill station/Darjeeling1.jpg';
+                hill10Elment.src='../images/hill-station/Darjeeling1.jpg';
             }
             else if(num ===2){
-                hill10Elment.src ='../images/Hill station/darjeeling4.webp';
+                hill10Elment.src ='../images/hill-station/darjeeling4.webp';
             }
             else if(num ===3){
-                hill10Elment.src ='../images/Hill station/darjeeling7.cms';
+                hill10Elment.src ='../images/hill-station/darjeeling7.cms';
             }
             else if(num === 4){
-                hill10Elment.src ='../images/Hill station/darjeeling2.jpg';
+                hill10Elment.src ='../images/hill-station/darjeeling2.jpg';
             }
             else if(num === 5){
-                hill10Elment.src ='../images/Hill station/darjeeling3.jpg';
+                hill10Elment.src ='../images/hill-station/darjeeling3.jpg';
             }
             else if(num === 6){
-                hill10Elment.src ='../images/Hill station/darjeeling5.jpeg';
+                hill10Elment.src ='../images/hill-station/darjeeling5.jpeg';
             }
         }
 
